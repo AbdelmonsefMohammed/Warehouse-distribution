649b51baf98088af86775fbb5779ffe01a7b02b2
Abdo <abdomohammed929@gmail.com>
2023-07-13 21:57:35 +0300

    clean up template

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This project is for self learning purposes and the code doesn't belong to any company 
 
 # prject structure
-    The project is splet into 4 main Services
+    The project is split into 4 main Services
     -  Identity Service
     -  Client Service
     -  Distribution Service
--- Services/template/app/Models/User.php
+++ Services/template/app/Models/User.php
@@ -1,16 +1,20 @@
 <?php
 
+declare( strict_types = 1 );
+
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\Role;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+final class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasUlids;
 
     /**
      * The attributes that are mass assignable.
@@ -30,7 +34,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -41,5 +44,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'role'  => Role::class,
     ];
 }
